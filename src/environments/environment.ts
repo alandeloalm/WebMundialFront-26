1361ad7e9d18301c448a88c8b9e71ac9a3f56784
Alan De Loa Larios <adeloa@tresxtres.com>
2026-03-09 16:47:49 -0600

    listo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    googleMapsApiKey: '',
+    googleMapsApiKey: 'AIzaSyBjfg8win0og-aIvGmGXujLwAz9TP-IdyY',
     apiUrl: '',
-    googleClientId: ''
+    googleClientId:"848433896921-pm51694akko95vhu4im8vhrri77mv802.apps.googleusercontent.com"
 };
