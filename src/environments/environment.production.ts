ebdb79d7d9074886a88e527f1531a44d570d650c
Alan De Loa Larios <adeloa@tresxtres.com>
2026-03-12 02:40:00 -0600

    eliminar environments del repo

--- src/environments/environment.production.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    googleMapsApiKey: 'AIzaSyBjfg8win0og-aIvGmGXujLwAz9TP-IdyY',
-    apiUrl: '',
-    googleClientId:"848433896921-pm51694akko95vhu4im8vhrri77mv802.apps.googleusercontent.com"
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    googleMapsApiKey: 'AIzaSyBjfg8win0og-aIvGmGXujLwAz9TP-IdyY',
-    apiUrl: 'http://localhost:3000/api',
-    googleClientId:"848433896921-pm51694akko95vhu4im8vhrri77mv802.apps.googleusercontent.com"
-};
